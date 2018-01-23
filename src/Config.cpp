b92bd7c272bc77aad4be0a516a4e6a42b6b698c6
Marek Poláček <Polda18@users.noreply.github.com>
2018-01-23 19:50:49 +0100

    Update Config.cpp
    Workspace update

--- src/Config.cpp
+++ src/Config.cpp
@@ -3,43 +3,43 @@
  * LICENCE: GNU-GPL-v3.1
  * 
  * File: Config.cpp
+ * Description: Configuration class for storing config data
  ***************************************************************/
 
 #include "Config.h"
 
-Config::Config(void) {
+wikibot::Config::Config(void) {
     wiki_server = nullptr;
     main_script = nullptr;
     definitions_file = nullptr;
 }
 
 // Get wiki server host name wikibot operates on
-std::string Config::get_wiki_server(void) {
+std::string wikibot::Config::get_wiki_server(void) {
     return wiki_server;
 }
 
 // Get wikibot main script full URL
-std::string Config::get_main_script(void) {
+std::string wikibot::Config::get_main_script(void) {
     return main_script;
 }
 
 // Get wikibot functions and commands JSON definition full URL
-std::string Config::get_definitions_file(void) {
+std::string wikibot::Config::get_definitions_file(void) {
     return definitions_file;
 }
 
 // Set wiki server host name wikibt operates on
-void Config::set_wiki_server(std::string hostname) {
+void wikibot::Config::set_wiki_server(std::string hostname) {
     wiki_server = hostname;
 }
 
 // Set wikibot main script full URL
-void Config::set_main_script(std::string fullurl) {
+void wikibot::Config::set_main_script(std::string fullurl) {
     main_script = fullurl;
 }
 
 // Set wikibot functions and commands JSON definition full URL
-void Config::set_definitions_file(std::string fullurl) {
+void wikibot::Config::set_definitions_file(std::string fullurl) {
     definitions_file = fullurl;
 }
-
