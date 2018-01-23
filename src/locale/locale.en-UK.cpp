cb5461b33116f86d66e3740752bdc9d403f62274
Marek Poláček <Polda18@users.noreply.github.com>
2018-01-23 19:46:50 +0100

    Update locale.en-UK.cpp
    Comments, file description

--- src/locale/locale.en-UK.cpp
+++ src/locale/locale.en-UK.cpp
@@ -3,10 +3,12 @@
  * LICENCE: GNU-GPL-v3.1
  * 
  * File: locale.en-UK.cpp
+ * Description: United Kingdom - English locale file
  ***************************************************************/
 
 #include "locale.h"
 
+// General locale strings
 wikibot::locale::general::done_capt = "Done";
 wikibot::locale::general::error_capt = "Error!";
 wikibot::locale::general::error_fopen = "Can't open file";
@@ -18,21 +20,26 @@ wikibot::locale::general::log_capt = "Log";
 wikibot::locale::general::log_error = "[ERROR]";
 wikibot::locale::general::log_run = "[PROTOCOL]";
 
+// Header locale strings
 wikibot::locale::header::program_name = "Wikibot Terminal Tool";
 wikibot::locale::header::version = "v0.1 BETA";
 
+// Check for server online status locale strings
 wikibot::locale::online_check::checking_wiki_server = "Checking wiki server online state";
 wikibot::locale::online_check::checking_main_script = "Checking wikibot main script online state";
 wikibot::locale::online_check::checking_json_define = "Checking wikibot JSON definition file online state";
 
+// Login session locale strings
 wikibot::locale::login::uname_capt = "Username > ";
 wikibot::locale::login::passw_capt = "Password > ";
 
+// Configuration session locale strings
 wikibot::locale::config::head_capt = "Wikibot configuration. Continue using instructions.\nRead manual for further details.";
 wikibot::locale::config::wiki_server_capt = "Define used wiki server > ";
 wikibot::locale::config::main_script_capt = "Define wikibot main script URL > ";
 wikibot::locale::config::json_define_capt = "Define wikibot JSON definition file URL > ";
 
+// Command loop session locale strings
 wikibot::locale::command_loop::prompt = "Enter command > ";
 wikibot::locale::command_loop::syntax_incorrect = "Entered command syntax is incorrect. Type 'help' for help topics.";
 wikibot::locale::command_loop::command_unknown = "Unknown command. Type 'help' for help topics.";
