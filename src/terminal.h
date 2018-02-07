19d3cbad15a38a2e8e2e5e2f7e63200c27b9add0
Marek Poláček <Polda18@users.noreply.github.com>
2018-02-07 08:51:05 +0100

    Create terminal.h
    Terminal input hide & show function for login session

---
+++ src/terminal.h
@@ -0,0 +1,18 @@
+/***************************************************************
+ * Wikibot Terminal Tool - by Polda18
+ * LICENCE: GNU-GPL-v3.1
+ * 
+ * File: login.h
+ * Description: Implementation of terminal input hide function
+ ***************************************************************/
+
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+namespace terminal
+{
+    void hide_input(void);
+    void show_input(void);
+}
+
+#endif
