a11b1d7b68494690c8e51fa939e64b2d551cc340
Marek Poláček <Polda18@users.noreply.github.com>
2018-02-07 08:56:10 +0100

    Create terminal.win32.cpp

---
+++ src/terminal.win32.cpp
@@ -0,0 +1,27 @@
+
+/***************************************************************
+ * Wikibot Terminal Tool - by Polda18
+ * LICENCE: GNU-GPL-v3.1
+ * 
+ * File: terminal.win32.cpp
+ * Description: Implementation of terminal input hide function
+ *              Windows version
+ ***************************************************************/
+
+#include <windows.h>
+
+#include "terminal.h"
+
+HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+DWORD mode = 0;
+
+terminal::hide_input(void)
+{
+    GetConsoleMode(hStdin, &mode);
+    SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+}
+
+terminal::show_input(void)
+{
+    SetConsoleMode(hStdin, mode);
+}
